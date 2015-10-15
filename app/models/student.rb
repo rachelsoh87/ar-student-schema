a0c1dfe0859582c0d284fdaf3a979d256ba07508
rachelsoh87 <ahsoh87@yahoo.com.my>
2015-10-15 14:53:36 +0000

    added validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,8 +2,11 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
-
-
+  validates :email, format: { with: /.+@.+[.][^.]{2,}\z/ , message: "please enter a valid email"}
+  validates :email, uniqueness: true
+  validates :age, numericality: { greater_than: 4 }
+  validates :phone, format: { with: /.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*\d.*/ }
+  
   def name
     self.first_name + " " + self.last_name
   end
